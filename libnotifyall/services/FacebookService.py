59855db11e03b6823a0d629f9be265a7f830cc4b
Pablo Seminario <pabluk@gmail.com>
2009-12-14 19:17:42 +0100

    Added support for Facebook notification feed

---
+++ libnotifyall/services/FacebookService.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Notify All - Copyright (c) 2009 Juan Pablo Seminario
+# This software is distributed under the terms of the GNU General
+# Public License
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from libnotifyall.message import Message
+from libnotifyall.service import Service
+import feedparser
+import ConfigParser
+import os, urllib2, time, sys
+
+class FacebookService(Service):
+
+    def load_config(self):
+
+        config = ConfigParser.ConfigParser()
+
+        config.read(self.configfile)
+        self.fbid = config.get("facebook", "id")
+        self.viewer = config.get("facebook", "viewer")
+        self.key = config.get("facebook", "key")
+        self.interval = int(config.get("facebook", "interval"))
+        self.feed_url = "http://www.facebook.com/feeds/notifications.php?id="+self.fbid+"&viewer="+self.viewer+"&key="+self.key+"&format=rss20"
+
+    def update(self):
+
+        print "[" + time.strftime("%H:%M") + "]",
+        print "[Facebook] Updating ...",
+        try:
+            a = feedparser.parse(self.feed_url)
+            print "[OK]"
+        except:
+            print "[ERROR]",
+            print "(You must verify your configuration)"
+            return 1
+
+        if len(a['entries']) > 0:
+            for entry in a['entries']:
+                message_exists = False
+                for message in self.messages:
+                    if message.id == entry.link:
+                        message_exists = True
+
+                if not message_exists:
+                    m = Message(entry.link, 'Facebook', 'Facebook', entry.title, os.getcwd() + "/icons/" + "facebook.png")
+                    self.messages.append(m)
+
--- notifyall.py
+++ notifyall.py
@@ -20,6 +20,7 @@
 
 from libnotifyall.services.TwitterService import TwitterService
 from libnotifyall.services.GmailService import GmailService
+from libnotifyall.services.FacebookService import FacebookService
 import os, time, sys
 
 from libnotifyall import CONFIG_DIR, CONFIG_FILE
@@ -58,12 +59,15 @@ if __name__ == "__main__":
     if not os.path.exists(CONFIG_DIR):
         create_initial_config()
 
-    s1 = TwitterService()
-    s2 = GmailService()
+    #s1 = TwitterService()
+    #s2 = GmailService()
+    s3 = FacebookService()
     while 1:
-        s1.update()
-        s1.show_messages()
-        s2.update()
-        s2.show_messages()
+        #s1.update()
+        #s1.show_messages()
+        #s2.update()
+        #s2.show_messages()
+        s3.update()
+        s3.show_messages()
         time.sleep(80)
 
