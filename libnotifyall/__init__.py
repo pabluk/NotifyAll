d3a7c526f33bf0c450d0da0e07f333a336c385af
Pablo Seminario <pabluk@gmail.com>
2009-12-16 15:24:24 +0100

    Updated vars in __init__.py files

--- libnotifyall/__init__.py
+++ libnotifyall/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+__all__ = ['NotifyAll', 'Service', 'Message']
+
 APP_NAME = "Notify All"
 CONFIG_DIR = os.path.expanduser('~') + "/" + ".notifyall"
 CONFIG_FILE = CONFIG_DIR + '/' + 'notifyall.cfg'
